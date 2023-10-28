080f93d48bdaeb520bd47b5fede3bc693561f1db
Paul Debus <debuspaul@gmail.com>
2023-10-28 15:30:06 +0200

    Implement Julia interface
    add the required packages for mesh loading and build the Julia side of
    the wrapper

--- src/LibAcc.jl
+++ src/LibAcc.jl
@@ -1,14 +1,39 @@
 module LibAcc
 
-# export greet
+using GeometryBasics
+
+export Scene, intersect
 
 using CxxWrap
-@wrapmodule(() -> joinpath(@__DIR__, "..", "extension", "build", "libacc"))
+@wrapmodule(() -> joinpath(@__DIR__, "..", "extension", "build", "libaccwrap"))
 
 function __init__()
 	@initcxx
 end
 
+Vec3f(x, y, z) = Vec3f(Float32(x), Float32(y), Float32(z))
 
+
+function BVHTree(mesh::Mesh)
+	faces = GeometryBasics.raw.(Iterators.flatten(GeometryBasics.faces(mesh)))
+	vertices = CxxRef.([Vec3f(coords...) for coords in GeometryBasics.coordinates(mesh)])
+	return BVHTree(faces, vertices)
+end
+
+function intersect(tree::BVHTree, origin, direction)
+	if length(direction) != 3 || length(origin) != 3
+		error("Invalid input dimensions, need 3D.")
+	end
+	o = Vec3f(origin...)
+	d = Vec3f(direction...)
+	return intersect(tree, o, d)
+end
+
+function closest_point(tree::BVHTree, point::Vector)
+	if length(point) != 3
+		error("Invalid input dimensions, need 3D.")
+	end
+	return data(closest_point(tree, Vec3f(point...)))
+end
 
 end # module LibAcc
